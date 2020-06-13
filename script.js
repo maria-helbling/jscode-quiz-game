2b63bc3071fa12a99f55de1c0b1d531a8511a7de
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-13 16:32:05 -0700

    added grid layout on page together with lifelines

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ let answrForm = document.querySelector('#answers');
 let quiz = document.querySelector('#quiz')
 let strtBtn = document.querySelector('#starter');
 let intro = document.querySelector('#intro');
+let clueBtn = document.querySelector('#clue');
+let Btn50 = document.querySelector('#50');
+let friendBtn = document.querySelector('#friend');
 let q;
 
+let questions = {
+    1: {q:`some q`,
+        a:[``,``,``,``],
+        correct: 1,
+        asked: false
+        clue:`some clue`
+        friend: 2,
+        5050:[2,3]},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {}
+}
+
 strtBtn.addEventListener('click',function(){
-    console.log("starting")
+
+    //hide intro text and button to reveal quiz text and answer buttons
     strtBtn.style.display = 'none';
     intro.style.display = 'none';
 
@@ -18,10 +41,24 @@ strtBtn.addEventListener('click',function(){
 let quizMaster = () => {
     qTitel.textContent = `Q${q}`;
     qText.textContent = `Here's a question for you with three answer options`;
+    let btn0 = document.createElement('button');
+    quiz.appendChild(btn0);
+    btn0.classList.add('btn');
+    btn0.classList.add('w-50');
+    btn0.id = '1';
+    btn0.textContent = `Answer`
     let btn1 = document.createElement('button');
     quiz.appendChild(btn1);
     btn1.classList.add('btn');
+    btn1.classList.add('w-50');
     btn1.id = '1';
     btn1.textContent = `Answer`
-    console.log(btn1);
+    let btn2 = document.createElement('button');
+    quiz.appendChild(btn2);
+    btn2.classList.add('btn');
+    btn2.classList.add('w-50');
+    btn2.id = '2';
+    btn2.textContent = `wrong answer`
+    
 }
+
